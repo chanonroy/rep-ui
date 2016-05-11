59cd01f841bd0f443a4898a2211a8a095739e502
Chanon Roy <chanonroy@gmail.com>
2016-05-11 13:54:50 -0400

    charts added. need to make DRY

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,31 @@
-var ctx = $("#myChart");
+var box1 = $("#box1");
+var box2 = $("#box2");
+var box3 = $("#box3");
+var box4 = $("#box4");
+var box5 = $("#box5");
+var box6 = $("#box6");
 
-var options = {
-  showLines: false,
-  
-};
+var options2 = {
+      responsive: true,
+      scales: {
+        yAxes: [{ display: false,}],
+        xAxes: [{ display: false,}]
+      },
+      gridlines: {
+        display: false,
+      },
+      legend: {display: false}
+    };
 
 var data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    labels: ["1", "2", "3", "4", "5", "6", "7", "8"],
     datasets: [
         {
             label: "My First dataset",
             fill: true,
             lineTension: 0.1,
-            backgroundColor: "rgba(75,192,192,0.4)",
-            borderColor: "rgba(75,192,192,1)",
+            backgroundColor: "rgba(75, 193, 90, 0.4)",
+            borderColor: "rgba(75, 193, 90, 1)",
             borderCapStyle: 'butt',
             borderDash: [],
             borderDashOffset: 0.0,
@@ -27,14 +39,43 @@ var data = {
             pointHoverBorderWidth: 2,
             pointRadius: 2,
             pointHitRadius: 10,
-            showLines: false,
-            data: [65, 59, 80, 81, 56, 55, 40],
+            data: [13, 16, 14, 15, 18, 20, 21, 25],
         }
     ]
 };
 
-var myLineChart = new Chart(ctx, {
+var chart1 = new Chart(box1, {
     type: 'line',
     data: data,
-    options: options,
+    options: options2,
+});
+
+var chart2 = new Chart(box2, {
+    type: 'line',
+    data: data,
+    options: options2,
+});
+
+var chart3 = new Chart(box3, {
+    type: 'line',
+    data: data,
+    options: options2,
+});
+
+var chart4 = new Chart(box4, {
+    type: 'line',
+    data: data,
+    options: options2,
+});
+
+var chart5 = new Chart(box5, {
+    type: 'line',
+    data: data,
+    options: options2,
+});
+
+var chart6 = new Chart(box6, {
+    type: 'line',
+    data: data,
+    options: options2,
 });
