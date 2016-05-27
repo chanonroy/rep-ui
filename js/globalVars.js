ec0e0cd0e501411e98801bdd56ba67ec76591341
Chanon Roy <chanonroy@gmail.com>
2016-05-27 09:45:22 -0400

    edited chart default placeholder

--- js/globalVars.js
+++ js/globalVars.js
@@ -14,5 +14,5 @@
   var twitterLikes = [];
 
 // default big chart values
-var emptyData = [];
-var dateDefault = [];
+var emptyData = [1000, 2500, 3000, 3250, 4000, 4500, 5000, 6000, 7500, 8000, 8500, 9500];
+var dateDefault = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
